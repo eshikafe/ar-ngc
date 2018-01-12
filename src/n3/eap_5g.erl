8d76b97572181fea671e6c932db7bbd2bca0be66
eshikafe <eshikafe@gmail.com>
2018-01-12 20:16:30 +0100

    test

--- src/n3/eap_5g.erl
+++ src/n3/eap_5g.erl
@@ -1,5 +1,7 @@
-%% 5G Core Network - Untrusted non-3GPP Access
+%% 5G Core Network - Untrusted non-3GPP Access control plane module
 %% SoC
 %%   3GPP TS 23.501 V15.0.0 (2017-12)
+%%   TS 33.402 V8.2.1 (2008-12)
+%%   EAP-AKA' https://tools.ietf.org/html/draft-arkko-eap-rfc5448bis-00
 
 -module(eap_5g).
