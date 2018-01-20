d570b2d045fb47d455833fe3b201cd7688c774df
eshikafe <eshikafe@gmail.com>
2018-01-20 07:36:45 +0100

    Add EAP-AKA' module
    TS 33.402 V8.2.1 (2008-12)
    EAP-AKA' https://tools.ietf.org/html/draft-arkko-eap-rfc5448bis-00

--- src/n3/eap_5g.erl
+++ src/n3/eap_5g.erl
@@ -4,4 +4,6 @@
 %%   TS 33.402 V8.2.1 (2008-12)
 %%   EAP-AKA' https://tools.ietf.org/html/draft-arkko-eap-rfc5448bis-00
 
+-define(PRINT(Var), io:format("~p:~p - ~p~n", [?MODULE, ?LINE, Var])).
+
 -module(eap_5g).
