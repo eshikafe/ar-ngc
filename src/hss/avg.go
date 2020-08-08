7acecc583b971e5635f19f19594ec90a84698b58
Austin Aigbe <eshikafe@gmail.com>
2020-08-08 14:59:28 +0100

    Add quintet generator function

--- src/hss/avg.go
+++ src/hss/avg.go
@@ -7,20 +7,37 @@ import "ngc/lib/milenage"
 
 // Quintet = (RAND, XRES, CK, IK, AUTN)
 type Quintet struct {
-	rand []byte
-	xres []byte // from milenage f2
-	ck   []byte // from milenage f3
-	ik   []byte // from milenage f4
-	autn []byte // SQN ^ AK || AMF|| MAC-A
+	rand []byte // (128-bit)
+	xres []byte // from milenage f2 (64-bit)
+	ck   []byte // from milenage f3 (128-bit)
+	ik   []byte // from milenage f4 (128-bit)
+	autn []byte // SQN ^ AK || AMF|| MAC-A (128-bit = 48||16||64)
 }
 
 // The A4 algorithm used to decrypt the encrypted user secret key is AES128
 // Sensitive user data is stored encrypted in the database, so it needs to be decrypted to be used in the generation of quintets
 
-func GenerateQuintet(ak, amf []byte) Quintet {
-	var q Quintet
+func GenerateQuintet(opc, k, rand, sqn, amf []byte) Quintet {
+	var quintet Quintet
+	tmp := make([]byte, 6)
+	autn := make([]byte, 16)
 
-	macA := milenage.F1()
-	xRes := milenage.F2()
+	ak := milenage.F5(opc, k, rand, sqn, amf)
+	mac := milenage.F1(opc, k, rand, sqn, amf)
 
+	// SQN ^ AK || AMF|| MAC-A
+	for i := 0; i < 6; i++ {
+		tmp[i] = sqn[i] ^ ak[i]
+	}
+	copy(autn[0:6], tmp[0:6])
+	copy(autn[6:8], amf[0:2])
+	copy(autn[8:16], mac[0:8])
+
+	quintet.rand = rand
+	quintet.xres = milenage.F2(opc, k, rand, sqn, amf)
+	quintet.ck = milenage.F3(opc, k, rand, sqn, amf)
+	quintet.ik = milenage.F4(opc, k, rand, sqn, amf)
+	quintet.autn = autn
+
+	return quintet
 }
