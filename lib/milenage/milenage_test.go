b15d77c195492d3f0d7631ef2b952a015aaa9405
Austin Aigbe <eshikafe@gmail.com>
2020-12-25 11:58:03 +0100

    Fixed Milenage F1 failed test set 1

--- lib/milenage/milenage_test.go
+++ lib/milenage/milenage_test.go
@@ -22,7 +22,7 @@ func strToHexArray(h string) []byte {
 	return n
 }
 
-// TestRijndaelTestSet1 implements Rijndael test date in
+// TestRijndaelTestSet1 implements Rijndael test data in
 // 3GPP TS 35.207 section 3.3 Test Set 1
 func TestRijndaelTestSet1(t *testing.T) {
 	// Key: 465b5ce8 b199b49f aa5f0a2e e238a6bc
@@ -32,6 +32,9 @@ func TestRijndaelTestSet1(t *testing.T) {
 	plaintext := strToHexArray("ee36f7cf037d37d3692f7f0399e7949a")
 	ciphertext := strToHexArray("9e2980c59739da67b136355e3cede6a2")
 	res := AESEncrypt(plaintext, key)
+
+	fmt.Printf("Rijndael (Actual)  : %x\n", res)
+	fmt.Printf("Rijndael (Expected): %x\n", ciphertext)
 
 	if !bytes.Equal(res, ciphertext) {
 		t.Error("3GPP TS 35.207 Rijndael Test Set 1 failed\n\n")
@@ -56,7 +59,7 @@ func TestF1TestSet1(t *testing.T) {
 	k := strToHexArray("465b5ce8b199b49faa5f0a2ee238a6bc")
 	rand := strToHexArray("23553cbe9637a89d218ae64dae47bf35")
 	amf := strToHexArray("b9b9")
-	opc := strToHexArray("cdc202d5123e20f62b6d676ac72cb318")
+	opc := strToHexArray("cd63cb71954a9f4e48a5994e37a02baf")
 	sqn := strToHexArray("ff9bb4d0b607")
 
 	// Value after 1st
@@ -71,15 +74,17 @@ func TestF1TestSet1(t *testing.T) {
 	//macSE := strToHexArray("01cfaf9ec4e871e9")
 	macAA := F1(opc, k, rand, sqn, amf)
 	//macSA := F1Star(opc, k, rand, sqn, amf)
+	e1 := strToHexArray("9e2980c59739da67b136355e3cede6a2")
+	fmt.Printf("1st encryption (expected)  : %x\n", e1)
 
-	fmt.Printf("f1(Actual)  : %x\n", macAA)
+	fmt.Printf("f1 (Actual)  : %x\n", macAA)
 	fmt.Printf("f1 (Expected): %x\n", macAE)
 
 	// fmt.Printf("f1* (Actual)  : %x\n", macSA)
 	// fmt.Printf("f1* (Expected): %x\n", macSE)
 	if !bytes.Equal(macAE, macAA) {
-		t.Error("3GPP TS 35.207 f1/f1* Test Set 1 failed\n")
+		t.Error("3GPP TS 35.207 Milenage f1/f1* Test Set 1 failed\n")
 	} else {
-		fmt.Print("3GPP TS 35.207 f1/f1* Test Set 1 Passed\n")
+		fmt.Print("3GPP TS 35.207 Milenage f1/f1* Test Set 1 Passed\n")
 	}
 }
